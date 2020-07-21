30b1188a55550f675d44768b7defada0c01351c4
Oleg <oleh.vysotskyi@gmail.com>
2020-07-21 08:44:36 +0300

    add guideline file

---
+++ pages/guideline.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="heading-1">Heading 1</h1>
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    textColor: {
+      primary: 'green'
+    }
+  },
   variants: {},
   plugins: [],
   purge: {
