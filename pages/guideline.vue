9a59813f82db4b34d0bf1418a3beda5269c991a8
Jacques <jacquesvandermerwe1974@gmail.com>
2020-07-22 22:49:19 +0300

    Init Nuxt

--- pages/guideline.vue
+++ pages/guideline.vue
@@ -1,5 +1,47 @@
 <template>
     <div>
-        <h1 class="heading-1">Heading 1</h1>
+      <h1 class="text-2xl">Heading 1</h1>
+      <h2 class="text-xl">Heading 2</h2>
+      <h3 class="text-m">Heading 3</h3>
+      <h4 class="text-sm">Heading 4</h4>
+
+      <hr class="my-8">
+
+      <p class="text-m">caption 1</p>
+      <p class="text-sm">caption 2</p>
+      <p class="text-sm">caption 3</p>
+      <p class="text-xs">caption 4</p>
+      <p class="text-xxs">caption 5</p>
+
+      <hr class="my-8">
+
+      <h4 class="text-sm">Main text 4</h4>
+      <p class="text-sm w-2/5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan posuere quam id mattis. Donec vel tortor libero. Suspendisse placerat, nibh sit amet tempor accumsan, massa lorem pretium mi, id vestibulum ante tortor porta turpis.</p>
+
+      <hr class="my-8">
+
+      <h4 class="text-sm">Bullet list</h4>
+      <div class="mx-5">
+        <ul class="text-xs list-disc">
+          <li>1st Item</li>
+          <li>2nd Item</li>
+          <li>3rd Item</li>
+          <li>4th Item</li>
+        </ul>
+      </div>
+
+      <hr class="my-8">
+
+      <h4 class="text-sm">Numbered list</h4>
+      <div class="mx-5">
+        <ul class="text-xs list-decimal">
+          <li>1st Item</li>
+          <li>2nd Item</li>
+          <li>3rd Item</li>
+          <li>4th Item</li>
+        </ul>
+      </div>
+
+
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,45 @@
 */
 module.exports = {
   theme: {
-    textColor: {
-      primary: 'green'
+
+    colors: {
+      primary: {
+        white: '#FFFFFF',
+        black: '#323232'
+      },
+
+      accent: {
+        purple: '#60198C',
+        blue: '#0A386A'
+      },
+
+      secondary: {
+        white100: '#F9F9F9',
+        white200: '#F8F6F6',
+        white300: '#FFF8F8',
+        grey100: '#F2F2F2',
+        grey200: '#C1C1C1',
+        grey300: '#747474',
+        hover: '#852BBD'
+      }
+
+    },
+
+    extend: {
+
+    },
+
+    fontFamily: {
+      serif: ['Proxima Nova', 'sans-serif']
+    },
+
+    fontSize: {
+      '2xl': '22px',
+      'xl': '18px',
+      'm': '16px',
+      'sm': '14px',
+      'xs': '12px',
+      'xxs': '11px'
     }
   },
   variants: {},
