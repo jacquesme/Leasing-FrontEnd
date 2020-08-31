7a7084c786a558e5dcb5406be3ecaa189a1ee5ef
Jacques <jacquesvandermerwe1974@gmail.com>
2020-08-31 08:54:10 +0300

    Install Scss, Use BEM, Load image static

--- components/GoodsCard.vue
+++ components/GoodsCard.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="goods-card inline-block">
-
-    <div class="goods-content">
-      <div class="goods-img-container">
-        <img class="goods-image" :src="content.jpeg">
-        <div class="middle">
-          <div class="pencil-container">
+  <div class="goods-card">
+    <div class="goods-card_content">
+      <div class="goods-card_img-container">
+        <img class="goods-card_image" src="~/assets/img/car.jpg"/>
+        <div class="goods-card_overlay">
+          <div class="goods-card_pencil-container">
             <a href="#">
-              <span class="ellipse1" v-html="content.svg"></span><span class="pencilIcon" v-html="pencilIcon.svg"></span>
+              <PencilEdit class="goods-card_pencil-icon"/>
             </a>
           </div>
-          <div class="rubbishBin-container">
+          <div class="goods-card_bin-container">
             <a href="#">
-              <span class="ellipse2" v-html="content.svg"></span><span class="rubbishBinIcon" v-html="rubbishBinIcon.svg"></span>
+              <RubbishBin class="goods-card_bin-icon"/>
             </a>
           </div>
         </div>
       </div>
 
-      <div class="bg-grey-100 p-4">
+      <div class="bg-grey-100 pt-4 px-4 pb-2">
         <h3 class="goods-name text-m text-black mb-2">{{ content.name }}</h3>
 
         <div class="grid grid-cols-1 md:grid-cols-5">
@@ -34,19 +33,102 @@
           <div class="col-span-3 md:ml-5">
             <button class="btn btn-purple">More details</button>
           </div>
-
         </div>
 
       </div>
     </div>
-
-
-
   </div>
 </template>
 
 <script>
+  import PencilEdit from '~/assets/svg/pencil-edit-button.svg?inline';
+  import RubbishBin from '~/assets/svg/rubbish-bin.svg?inline';
+
   export default {
-    props: ['content', 'pencilIcon', 'rubbishBinIcon']
+    components: {PencilEdit, RubbishBin},
+    props: ['content']
   }
 </script>
+
+<style lang="scss" scoped>
+  .goods-card {
+    display: inline-block;
+    border-radius: 0.375rem;
+    overflow: auto;
+    &_content {
+      background-color: #F2F2F2;
+      width: 300px;
+      height: auto;
+      .goods-card_img-container {
+        position: relative;
+        width: 300px;
+        height: 240px;
+        &:after {
+          content: '\A';
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          background-color: rgba(0,0,0,0.4);
+          opacity: 0;
+          transition: all;
+          transition-duration: 1000ms;
+          border-top-left-radius: 0.375rem;
+          border-top-right-radius: 0.375rem;
+          -webkit-transition: all 1s;
+        }
+        .goods-card_image {
+
+        }
+        .goods-card_overlay {
+          transition: all;
+          transition-duration: 500ms;
+          opacity: 0;
+          position: absolute;
+          text-align: center;
+          top: 15%;
+          left: 73%;
+          transform: translate(-20%, -55%);
+          .goods-card_pencil-container {
+            position: relative;
+            display: inline-block;
+            margin-left: 0.5rem;
+            border: 1px solid white;
+            border-radius: 50%;
+            width: 30px;
+            height: 30px;
+            .goods-card_pencil-icon {
+              position: absolute;
+              top: 24%;
+              left: 25%;
+            }
+          }
+          .goods-card_bin-container {
+            position: relative;
+            display: inline-block;
+            margin-left: 0.5rem;
+            border: 1px solid white;
+            border-radius: 50%;
+            width: 30px;
+            height: 30px;
+            .goods-card_bin-icon {
+              position: absolute;
+              top: 24%;
+              left: 30%;
+            }
+          }
+
+        }
+        &:hover:after {
+          opacity: 1
+        }
+        &:hover .goods-card_overlay {
+          opacity: 1;
+          z-index: 10;
+        }
+      }
+
+    }
+  }
+</style>
--- components/Menu.vue
+++ components/Menu.vue
@@ -1,11 +1,11 @@
 <template>
 
-  <div class="menu grid sm:ml-0">
+  <div class="menu">
     <div v-for="item in items">
-      <div class="menu-item"><a href="#"><span class="svg" v-html="item.svg"></span> {{ item.name }}</a></div>
+      <div class="menu-item"><a href="#"><span class="menu-item_svg" v-html="item.svg"></span> {{ item.name }}</a></div>
     </div>
     <div class="menu-logOff">
-      <div class="item-logOff"><a href="#">{{ logOff.name }}<span class="svg-logOff" v-html="logOff.svg"></span></a></div>
+      <div class="menu-logOff_item"><a href="#">{{ logOff.name }}<span class="menu-logOff_item_svg" v-html="logOff.svg"></span></a></div>
     </div>
   </div>
 
@@ -20,5 +20,48 @@
   }
 
 </script>
+
+
+<style lang="scss" scoped>
+  .menu {
+    background-color: white;
+    border-radius: 0.5rem;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    color: black;
+    font-size: 14px;
+    padding-top: 1.5rem;
+    width: 180px;
+    &-item {
+      margin-top: 1rem;
+      margin-left: 1.5rem;
+      &_svg {
+        display: inline-block;
+        margin-right: 1rem;
+        height: 0.75rem;
+        vertical-align: middle;
+        width: 0.75rem;
+      }
+    }
+    &-logOff {
+      background-color: #F2F2F2;
+      margin-top: 1.8rem;
+      padding-bottom: 5px;
+      &_item {
+        margin-bottom: 6px;
+        margin-left: 1.5rem;
+        margin-top: 1.5rem;
+        padding-top: 12px;
+        &_svg {
+          display: inline-block;
+          height: 1rem;
+          margin-left: 1rem;
+          vertical-align: middle;
+          width: 1rem;
+        }
+      }
+    }
+  }
+
+</style>
 
 
