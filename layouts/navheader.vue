f0aa68691c907b36e3605b13db2628da81f2e13c
Jacques <jacquesvandermerwe1974@gmail.com>
2020-09-23 16:20:28 +0300

    Created header layout, created homepage

---
+++ layouts/navheader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <NavMenu/>
+    </div>
+</template>
+
+<script>
+  import NavMenu from '~/components/Menu.vue';
+    export default {
+      components: {NavMenu},
+        name: "header"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/homepage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header-container center">
+        <NavHeader/>
+    </div>
+</template>
+
+<script>
+  import NavHeader from '~/layouts/navheader.vue';
+    export default {
+        components: {NavHeader},
+        name: "homepage"
+    }
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+  margin: 0;
+  width: 1200px;
+  background-color: #3b8070;
+}
+</style>
