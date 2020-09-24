175fd7c678ad0d903877c0e6d7c82c3217fc5d77
Jacques <jacquesvandermerwe1974@gmail.com>
2020-09-24 19:04:37 +0300

    Created header layout, created homepage

---
+++ components/SearchBar.vue
@@ -0,0 +1,107 @@
+<template>
+  <form class="mt-8 md:mt-0 md:flex md:items-center md:justify-between md:flex-wrap md:p-6">
+    <div class="md:flex md:items-center md:flex-shrink-0 md:text-white md:mr-6">
+      <Logo class="logo inline"/>
+      <span class="ml-4 text-2xl text-accent-purple">AutoLizing</span>
+      <button class="hamburger md:hidden" type="button" @click="menuOpen = !menuOpen">
+        <div class="hamburger-line  bg-grey-600"></div>
+        <div class="hamburger-line  bg-grey-600"></div>
+        <div class="hamburger-line  bg-grey-600"></div>
+      </button>
+      <div class="dropdown mt-6 md:hidden" :class="{ 'dropdown-after' : menuOpen }">
+        <input type="text" placeholder="Vehicle search" class="dropdown-search w-8/12 bg-grey-200 p-2 rounded-md inline-block text-black">
+        <button class="bg-accent-purple inline-block btn-search"><Search class="btn-search_icon"/></button>
+      </div>
+    </div>
+    <div class="w-full block md:flex-grow md:flex md:items-center md:w-auto">
+      <div class="text-xs hidden md:block md:flex-grow">
+        <input type="text" placeholder="Vehicle search" class="big-input bg-grey-200 px-4 h-md-40 rounded-l">
+        <button class="bg-accent-purple  btn-search"><Search class="btn-search_icon"/></button>
+      </div>
+      <div class="hidden md:block">
+        <button class="btn btn-purple">To add an advert</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+  import Search from '~/assets/svg/search.svg?inline';
+  import Logo from '~/assets/svg/logo.svg?inline';
+    export default {
+      components: {Logo, Search},
+      name: "SearchBar",
+      data() {
+        return {
+          menuOpen: false
+        }
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .hamburger {
+    position: absolute;
+    margin-left: 3rem;
+    margin-top: 9px;
+    cursor: pointer;
+  &:focus {
+     outline: none;
+   }
+  &-line {
+     margin-bottom: 8px;
+     width: 30px;
+     height: 2px;
+   }
+  &-line:last-child {
+     margin-bottom: 0;
+   }
+  }
+  .dropdown {
+    height: 0px;
+    width: 104%;
+    transition: height 0.5s ease;
+    overflow: hidden;
+  &-after {
+     height: calc(25vh - 50px);
+     transition: height 0.5s ease;
+   }
+  &-search {
+     border-radius: 0.25rem 0 0 0.25rem;
+  &::placeholder {
+     color: black;
+     opacity: 1;
+   }
+  &:focus {
+     outline: none;
+   }
+  }
+  }
+  .btn-search {
+    height: 40px;
+    width: 40px;
+    box-sizing: border-box;
+    vertical-align: top;
+    border-radius: 0 0.25rem 0.25rem 0;
+    margin-left: -6px;
+  &:hover {
+     background-color: #852BBD;
+     transition: ease-in-out 0.4s;
+   }
+  &_icon {
+     margin-left: 11px;
+   }
+  }
+  .big-input {
+    color: black;
+    margin-left: 6%;
+    width: 75%;
+  &::placeholder {
+     color: black;
+     opacity: 1;
+   }
+  &:focus {
+     outline: none;
+   }
+  }
+</style>
--- pages/homepage.vue
+++ pages/homepage.vue
@@ -1,21 +1,45 @@
 <template>
-    <div class="header-container center">
-        <NavHeader/>
+  <div class="bg-grey-400">
+    <div class="home-container w-10/12 md:w-75 m-auto bg-white">
+      <div class="hidden md:block">
+        <Nav/>
+      </div>
+      <div class="ml-8 md:hidden">
+          <SearchBar/>
+      </div>
+
+      <section class="banner-section bg-grey-300 grid md:grid-cols-2 px-20 py-6">
+        <div class="inline-block w-11/12 md:w-9/12 mt-8">
+          <h1 class="font-bold text-4xl">Autolizing</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet tincidunt libero. Duis.</p>
+          <button class="btn btn-purple mt-4">Search product</button>
+        </div>
+        <div class="hidden md:inline-block">
+          <img src="../assets/img/audiQ5.png" alt="audiQ5" class="audi w-4/5 h-auto">
+        </div>
+      </section>
+
+      <aside>
+
+      </aside>
+
     </div>
+  </div>
+
 </template>
 
 <script>
-  import NavHeader from '~/layouts/navheader.vue';
+  import Nav from '~/layouts/nav.vue';
+  import SearchBar from '~/components/SearchBar.vue';
+  import AudiQ5 from '~/assets/img/audiQ5.png';
     export default {
-        components: {NavHeader},
+        components: {Nav, SearchBar,AudiQ5},
         name: "homepage"
     }
 </script>
 
 <style lang="scss" scoped>
-.header-container {
-  margin: 0;
-  width: 1200px;
-  background-color: #3b8070;
+.home-container {
+
 }
 </style>
